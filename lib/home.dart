e29caf3008caf8ce6a85c69183de4a70d309c021
Ayusler <ayusler@gmail.com>
2021-05-31 13:37:43 +0100

    Done working with home file

---
+++ lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:task/time.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: Text(
+              "+",
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: (){
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>Time()));
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
